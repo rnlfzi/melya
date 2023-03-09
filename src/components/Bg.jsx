632497f7b525aacbe53377073534eb13d03330b2
rnlfzi <rfaidzilah@gmail.com>
2023-03-09 12:11:05 +0700

    up

--- src/components/Bg.jsx
+++ src/components/Bg.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Bg = () => {
   return (
-    <div className="" style={{ position: "fixed", top: '0', left: "0", }}>
-      <img style={{ width: "100%", objectFit: "cover", objectPosition: "center"}} src="/images/bg.jpg" alt="bg" />
+    <div className="" style={{ height: "100vh", position: "fixed", top: '0', left: "0", }}>
+      <img style={{ height: "100%", objectFit: "cover", objectPosition: "center"}} src="/images/bg.jpg" alt="bg" />
     </div>
   )
 }
